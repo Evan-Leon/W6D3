577f615dc40f2e7221f831b863418f60365fe6bf
Evan-Leon <evan.leon.j@gmail.com>
2021-03-31 16:00:17 -0400

    Finished users controller

--- art_share/app/controllers/users_controller.rb
+++ art_share/app/controllers/users_controller.rb
@@ -6,7 +6,40 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    user = User.find(params[:id])
+    render json: user 
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+        render json: user 
+    else  
+        render json: song.errors.full_messages, status: 422 
+    end
+        
+  end
+
+  def update 
+    user = User.find(params[:id])
+
+     if user.update(user_params)
+        redirect_to user_url(user) 
+    else  
+        render json: song.errors.full_messages, status: 422
+    end
+
+  end
+
+  def destroy 
+    user = User.find(params[:id])
+    user.destory
+    redirect_to users_url 
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 
 end
