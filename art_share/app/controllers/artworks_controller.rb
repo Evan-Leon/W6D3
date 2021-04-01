8742839fc89e570656bbedc5a9f630ed26a1ef65
juliajykim <jkim24@gwu.edu>
2021-04-01 12:01:22 -0400

    Complete artworks_controller

--- art_share/app/controllers/artworks_controller.rb
+++ art_share/app/controllers/artworks_controller.rb
@@ -1,30 +1,48 @@
 class ArtworksController < ApplicationController
 
     def index 
-        user = User.find(params[:id])
-
-        render json: user.artworks 
+        # user = User.find(params[:id])
+        artworks = Artwork.all
+        render json: artworks 
     end
 
-    def show 
-
+    def show
+        artwork = Artwork.find(params[:id])
+        render json: artwork 
     end
 
-    def create
-
+  def create
+    artwork = Artwork.new(artwork_params)
+    # debugger
+    if artwork.save
+        render json: artwork 
+    else  
+        render json: artwork.errors.full_messages, status: 422 
     end
+        
+  end
 
-    def update 
-
+  def update 
+    artwork = Artwork.find(params[:id])
+        # debugger
+     if artwork.update(artwork_params)
+        # debugger
+        # redirect_to artwork_url(artwork)
+        render json: artwork
+    else  
+        render json: artwork.errors.full_messages, status: 422
     end
+  end
 
-    def destroy 
+  def destroy 
+    artwork = Artwork.find(params[:id])
+    artwork.destroy
+    redirect_to artworks_url 
+  end
 
-    end
+  protected
+  def artwork_params
+    params.require(:artwork).permit(:title, :image_url, :artist_id)
+  end
 
-    protected
-
-    def artwork_params
-
-    end
 end
